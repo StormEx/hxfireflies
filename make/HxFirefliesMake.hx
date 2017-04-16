d7e7f39eeceaa1c4c3ebba87ae61916d7ad6c92d
StormEx <stormbox@tut.by>
2017-04-16 17:14:22 +0300

    added main/make files

---
+++ make/HxFirefliesMake.hx
@@ -0,0 +1,40 @@
+package ;
+
+import hxmake.haxelib.HaxelibExt;
+import hxmake.test.TestTask;
+import hxmake.idea.IdeaPlugin;
+import hxmake.haxelib.HaxelibPlugin;
+
+using hxmake.haxelib.HaxelibPlugin;
+
+class HxFirefliesMake extends hxmake.Module {
+	function new() {
+		config.classPath = ["src"];
+		config.testPath = ["test"];
+		config.devDependencies = [
+			"utest" => "haxelib"
+		];
+
+		apply(HaxelibPlugin);
+		apply(IdeaPlugin);
+
+		library(
+			function(ext:HaxelibExt) {
+				ext.config.description = "Simple particles";
+				ext.config.contributors = ["storm_ex"];
+				ext.config.url = "https://github.com/StormEx/hxfireflies";
+				ext.config.license = "MIT";
+				ext.config.version = "0.0.1";
+				ext.config.releasenote = "Initial release";
+				ext.config.tags = ["particles"];
+
+				ext.pack.includes = ["src", "haxelib.json", "README.md"];
+			}
+		);
+
+		var tt = new TestTask();
+		tt.targets = ["neko", "swf", "js", "node", "cpp", "java", "cs", "php"];
+		tt.libraries = ["hxfireflies"];
+		task("test", tt);
+	}
+}
